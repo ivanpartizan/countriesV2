38c6eb203d4173e6a4123564978a55f1c811ab44
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-01-15 04:52:20 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://restcountries.com/v3.1/all";
+const url = "https://restcountries.com/v2/all";
 
 let data = [];
 const search = document.getElementById("searchbar");
@@ -21,26 +21,26 @@ const displayData = (data) => {
   document.getElementById("output").innerHTML = `${data
     .map(
       (country) =>
-        `<div class='country'> <h1 class='name'>${country.name.common}</h1> 
-        <h3 class='native'>(${country.name.official})</h3>
-        <h3 class='capital'>Capital: ${country.capital}</h3> 
+        `<div class='country'> <h1 class='name'>${country.name}</h1> 
+        <h3 class='native'>(${country.nativeName})</h3>
+        <h3 class='capital'>Capital: ${
+          country?.capital ? country.capital : "No Capital"
+        }</h3> 
         <h3 class='area'>Area: ${country.area} km²</h3>
         <h3 class='population'>Population: ${country.population}</h3>
-        <img src=${country.flags.png} height='50' class='flag' />
+        <img src=${country.flags.svg} height='50' class='flag' />
         <h2 class='region'>(${country.region})</h2>
         <h3 class='borders'>Borders: ${
-          country ?? borders.join(", ")
-            ? country ?? borders.join(", ")
-            : "water"
+          country?.borders ? country.borders.join(", ") : "water"
         }</h3>
-        <h3 class='languages'>Languages: ${Object.values(country.languages)
-          .map((name) => name)
+        <h3 class='languages'>Languages: ${country.languages
+          .map((lang) => lang.name)
           .join(", ")}</h3>
-        <h3 class='currency'>Currency: ${Object.keys(country.currencies).join(
-          ", "
-        )}</h3>
+        <h3 class='currency'>Currency: ${
+          country?.currencies ? country.currencies[0].name : "No Currency"
+        }</h3>
         <h3 class='timezone'>Timezone: ${country.timezones.join(", ")}</h3>
-        <h3 class='internet'>Internet Domain: ${country.tld[0]}</h3>
+        <h3 class='internet'>Internet Domain: ${country.topLevelDomain[0]}</h3>
         </div><br>`
     )
     .join("")} `;
